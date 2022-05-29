f618244684c2155116d05353f43f82fe1557cdd3
Himanshu Patel <Himanshu.Patel.26@gmail.com>
2022-05-29 13:40:40 -0700

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,8 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
 console.log(40 + 8 + 23 - 13);
+console.log('Himanshu');
+console.log(23);
+let firstName = 'Matilda';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
