e58d94bcf321c30a7944fc00697a0d9b7d4ce65a
Himanshu Patel <himanshu.patel.26@gmail.com>
2022-05-29 23:52:59 -0700

    let, var and const

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,6 @@
+let country = "India";
+let continent = "Asia";
+let population = "1400 M";
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 13);
 console.log('Himanshu');
@@ -6,3 +7,40 @@ let firstName = 'Matilda';
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+console.log("Himanshu");
+*/
+
+// Data Types
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof 20);
+console.log(typeof "Himanshu");
+console.log(typeof 'Himanshu');
+
+javascriptIsFun = 'Yes!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1992;
+console.log(typeof year);
+console.log(typeof null);
+*/
+
+// let, const, and var
+
+let age = 30;
+age = 31;
+
+const birthYear = 1986;
+// birthYear = 1987;
+
+var job = "programmer";
+job = "manager";
+
+lastName = 'Patel';
+console.log(lastName);
